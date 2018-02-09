d0c95872dd106b886a871331628a88832d4eb4c4
Mikko Vihtakari <mikko.vihtakari@gmail.com>
2018-02-09 16:58:57 +0100

    Added two functions to compile ice-core species data. Untested.

---
+++ R/compile_species_data.R
@@ -0,0 +1,263 @@
+#' @title Compile species data from ice cores
+#' @description Compiles species data across a larger dataset with duplicate species names as defined by a species list from \code{\link{confirm_species_list}} function. 
+#' @param dat data frame containing ice core species data to be combined. Must be in long format.
+#' @param splist species list. Preferably from \code{\link{confirm_species_list}}.
+#' @param core_cols A character vector specifying columns that separate individual ice-cores.
+#' @param sp_col Character specifying the name of the species column in \code{dat}. 
+#' @param ab_col Character specifying the name of the abundance column in \code{dat}. 
+#' @param unit_col Character specifying the name of the unit for the abundance column in \code{dat}. 
+#' @param start_col Character argument specifying the name of the column in \code{dat} for the section start for the ice core in centimeters or meters.
+#' @param end_col Character argument specifying the name of the column in \code{dat} for the section end for the ice core in centimeters or meters.
+#' @param ice_thick_col Character argument specifying the name of the column in \code{dat} for ice thickness preferably in \strong{centimeters}
+#' @param add_cols A character vector containing names of the additional columns that should be included in the output. These columns are not used by the function otherwise. Additional columns cannot contain several unique values due to summarizing process.
+#' @param convert_unit List or \code{NULL}. See details.
+#' @param summarize_cores Logical. Should core sections be summarized to a continuous core? If \code{TRUE} (default), all sections are summarized, \code{start_col} and \code{end_col} removed from the output, and a \code{$core_type} column added. If \code{FALSE} core sections are returned as they are.
+#' @param return_summary Logical. Should summary statistics be returned?
+#' @details Built for ice-algae taxonomy data, but could be modified to work with any taxonomy data including plankton nets. The function is currently broken for wider use.
+#' 
+#' \strong{convert_unit} argument:  Should the function convert supplied abundances using the \code{\link{convert_abundace}} function, a list giving \code{from} and \code{to} units have to be supplied. If conversion should not be made, use \code{NULL} (default). In this cases abundances are summed up for duplicate species. If the values are percentages, use \code{list(from = "per", to = "per")}. If the values are relative values (have to have a scale from 1, least abundant, to 5, most abundant), use \code{list(from = "rel", to = "rel")}
+#' @return Returns a data frame with columns specified in arguments. Drops any unspecified column.
+#' @importFrom plyr mapvalues
+#' @importFrom lazyeval interp
+#' @import dplyr
+#' @author Mikko Vihtakari
+#' @export
+#dat <- y[[2]]
+
+# sp_col = "species"; core_cols = c("expedition", "station", "core.id"); unit_col = "unit"; convert_unit = list(from = "rel", to = "rel"); start_col = "from"; end_col = "to"; ice_thick_col = "ice"; ab_col = "abundance"; summarise_cores = TRUE; add_cols = c("gear", "longitude", "latitude", "date", "bottom_depth", "ice.type", "snow"); return_summary = TRUE
+
+compile_species_data <- function(dat, splist, core_cols = c("expedition", "station", "core.id"), sp_col = "species", ab_col = "abundance", unit_col = "unit", start_col = "from", end_col = "to", ice_thick_col = "ice", add_cols = c("gear", "longitude", "latitude", "date", "bottom_depth", "ice.type", "snow"),  convert_unit = list(from = "1/L", to = "1/m2"), summarise_cores = TRUE, return_summary = TRUE) {
+
+### Tests ####
+
+req_splist_cols <- c("or_name", "use_name", "drop", "type", "certainty")
+
+if(length(unique(dat[unit_col])) != 1) stop("Only one unit type is allowed. Split ", unit_col, " column")
+if(!all(req_splist_cols %in% colnames(splist))) stop("splist (species list) must contain ", paste(req_splist_cols[!req_splist_cols %in% colnames(splist)], collapse = ", "), " columns. Easiest way to get these is to use confirm_species_list function.")
+
+## Split to unique ice cores
+
+dat$temp <- apply(dat, 1, function(k) paste(k[core_cols], collapse = "_"))
+
+dat <- dat[c("temp", core_cols, start_col, end_col, ice_thick_col, sp_col, unit_col, ab_col, add_cols)]
+
+x <- split(dat, dat$temp, drop = TRUE)
+
+## Loop ####
+
+# k <- x[["AARI_SHEBA_12"]]
+# k <- x[[2]]
+out <- lapply(x, function(k) {
+
+## Definitions for IDs
+id <- unique(k$temp)
+
+ #print(id)  
+
+k_add <- unique(k[c(core_cols, add_cols)])
+if(nrow(k_add) > 1) stop(">= 1 unique add_col entries for ", id, ". Not allowed")
+
+k <- k[!names(k) %in% c(add_cols, "temp")]
+
+k <- droplevels(k)
+
+## Definitions for ice cores
+
+### Test whether the ice core contains missing pieces
+sections <- unique(k[c(start_col, end_col)])
+cont_sec <- interleave(sections[[1]], sections[[2]])
+cont_sec <- all(duplicated(cont_sec[c(-1, -length(cont_sec))]) == rep(c(FALSE, TRUE), (length(cont_sec)-2)/2))
+
+### Parameters
+start <- min(k[start_col])
+end <- max(k[end_col])
+icethick <- unique(k[[ice_thick_col]])
+
+
+if(summarise_cores) {
+bottom.code <- bottom_sec(start, end, icethick, cont_sec)  
+}
+
+## Fix instances with empty cores
+
+z <- split(k, k[[start_col]])
+
+z <- lapply(z, function(j) {
+  if(nrow(j) == 1) {
+    if(is.na(j[[sp_col]]) & is.na(j[[ab_col]])) {
+    j[[sp_col]] <- "Empty"
+    j[[ab_col]] <- 0
+    j
+    } else {
+      j
+    }} else{
+    j
+  }
+})
+
+k <- do.call(rbind, z)
+
+## Remove high abundances of symbionts (M. rubrum)
+
+symbs <- splist[splist$type %in% "symbiont" & splist$use_name %in% "Mesodinium rubrum", "or_name"]
+
+if(any(levels(k[[sp_col]] %in% symbs))) {
+  k[k[[sp_col]] %in% symbs, ab_col] <- 1
+}
+
+## Rename species
+k_og <- k
+k[[sp_col]] <- factor(plyr::mapvalues(as.character(k[[sp_col]]), splist$or_name, splist$use_name, warn_missing = FALSE))
+
+k <- droplevels(k)
+
+## Convert units
+
+if(!is.null(convert_unit) & ((convert_unit$from != "per" & convert_unit$to != "per") & (convert_unit$from != "rel" & convert_unit$to != "rel"))) {
+  k <- suppressMessages(convert_abundance(data = k, ab_col = ab_col, ab_from = convert_unit$from, ab_to = convert_unit$to, filtered = TRUE))
+}
+
+######### MEGA MIND-FUCK #################
+## Sum up abundances for duplicate species
+
+if(convert_unit$from == "per" & convert_unit$to == "per") {
+  
+  if(summarise_cores) {
+    k <- k %>% dplyr::group_by_(.dots = names(k)[!names(k) %in% c(ab_col, start_col, end_col)]) %>% dplyr::summarise_(abundance = lazyeval::interp(~sum(var), var = as.name(ab_col)))
+    k <- data.frame(k)
+    if(ab_col != "abundance") {
+      names(k)[names(k) == "abundance"] <- ab_col
+    }
+    k[ab_col] <- 100*k[ab_col]/sum(k[[ab_col]])
+    k$core_type <- bottom.code
+    
+    if(nrow(k) > 1) k <- k[!k[[sp_col]] %in% "Empty",]
+    
+  } else {
+    k <- k %>% dplyr::group_by_(.dots = names(k)[!names(k) %in% c(ab_col)]) %>% dplyr::summarise_(abundance = lazyeval::interp(~sum(var), var = as.name(ab_col)))
+    k <- data.frame(k)
+    if(ab_col != "abundance") {
+      names(k)[names(k) == "abundance"] <- ab_col
+    }
+    k[ab_col] <- 100*k[ab_col]/sum(k[[ab_col]])
+}
+  
+if(round(sum(k[[ab_col]]), 0) != 100) stop("Abundace sum for ", id, " is not 100%")
+    
+} else if(convert_unit$from == "rel") {
+  
+  if(summarise_cores) {
+     k <- k %>% dplyr::group_by_(.dots = names(k)[!names(k) %in% c(ab_col, start_col, end_col)]) %>% dplyr::summarise_(abundance = lazyeval::interp(~sum(var), var = as.name(ab_col)), occurences = lazyeval::interp(~length(var), var = as.name(sp_col)))
+  } else {
+    k <- k %>% dplyr::group_by_(.dots = names(k)[!names(k) %in% c(ab_col)]) %>% dplyr::summarise_(abundance = lazyeval::interp(~sum(var), var = as.name(ab_col)), occurences = lazyeval::interp(~length(var), var = as.name(sp_col)))
+  }
+ 
+  k <- data.frame(k)
+  if(nrow(k) > 1 & !summarise_cores) k <- k[!k[[sp_col]] %in% "Empty",]
+  
+  if(convert_unit$to == "rel") {
+  temp_ab <- ifelse(k$occurences <= 1, k$abundance, ifelse(k$abundance == 0, 0, round(k$abundance/k$occurences, 0)))  
+  k[unit_col] <- "rel"
+  } else if(convert_unit$to == "per") {
+  temp_ab <- ifelse(k$abundance == 1, 0.1, 100*k$abundance/sum(k$abundance))
+  temp_ab <- 100*temp_ab/sum(temp_ab)
+  k[unit_col] <- "per"
+  } else {
+    stop("Abundace conversion for ", id, " not implemented")
+  }
+  
+  if(ab_col != "abundance") {
+      names(k)[names(k) == "abundance"] <- ab_col
+  }
+  
+  k[ab_col] <- temp_ab
+  
+  k <- k[!names(k) %in% "occurences"]
+  
+  if(summarise_cores) {
+     k$core_type <- bottom.code
+  }
+  
+} else {
+   if(summarise_cores) {
+    k <- k %>% dplyr::group_by_(.dots = names(k)[!names(k) %in% c(ab_col, start_col, end_col)]) %>% dplyr::summarise_(abundance = lazyeval::interp(~sum(var), var = as.name(ab_col)))
+    k <- data.frame(k)
+    if(ab_col != "abundance") {
+      names(k)[names(k) == "abundance"] <- ab_col
+    }
+    k$core_type <- bottom.code
+  } else {
+    k <- k %>% dplyr::group_by_(.dots = names(k)[!names(k) %in% c(ab_col)]) %>% dplyr::summarise_(abundance = lazyeval::interp(~sum(var), var = as.name(ab_col)))
+    k <- data.frame(k)
+    if(ab_col != "abundance") {
+      names(k)[names(k) == "abundance"] <- ab_col
+    }
+  }
+}
+  
+### Return
+
+if(summarise_cores) {
+  k <- merge(k, k_add, by = core_cols, all = TRUE, sort = FALSE)
+  k <- k[c(core_cols, add_cols, ice_thick_col, "core_type", sp_col, unit_col, ab_col)]
+  list(data = k, sumInfo = sum_info(ID = id, dt_st = k_og, dt_ed = k, sum_cor = summarise_cores, secs = sections))
+} else {
+  k <- merge(k, k_add, by = core_cols, all = TRUE, sort = FALSE)
+  k <- k[c(core_cols, add_cols, ice_thick_col, start_col, end_col, sp_col, unit_col, ab_col)]
+  list(data = k, sumInfo = sum_info(ID = id, dt_st = k_og, dt_ed = k, sum_cor = summarise_cores, secs = sections))
+}
+
+
+})
+### End loop ###
+### Return
+
+out_dat <- do.call(rbind, lapply(out, function(k) k$data))
+rownames(out_dat) <- 1:nrow(out_dat)
+
+if(return_summary) {
+  list(data = out_dat, sumInfo = lapply(out, function(k) k$sumInfo))
+} else {
+  out_dat
+}
+
+}
+#### Function end ####
+
+### Summary info function ####
+sum_info <- function(ID = id, dt_st = k_og, dt_ed = k, sum_cor = summarise_cores, secs = sections) {
+  paste0(ID, ": summarized from ", length(dt_st$species), " species to ", length(dt_ed$species), 
+  ". ", length(unique(dt_st$species)), " unique species at the beginning and ", length(unique(dt_st$species)), " at the end. ",
+  if(sum_cor) {
+  paste0("Cores summed. Core type: ", unique(k$core_type), ".")
+  } else {
+  paste("Cores kept separate. Sections:", paste(secs$from, secs$to, sep = "-", collapse = ", "))     
+    } 
+  )
+}
+
+### Define core type ####
+bottom_sec <- function(start, end, icethick, cont_sec) {
+  ifelse(all(start == 0, end == icethick, cont_sec), 
+    "whole core",
+  ifelse(all(start == 0, end == icethick, !cont_sec),
+    "whole core, gap",
+  ifelse(start == 0 & cont_sec,
+    paste("bottom", end, "cm"),
+  ifelse(start == 0 & !cont_sec,  
+    paste("bottom", end, "cm, gap"),
+  ifelse(start != 0,
+    "bottom sec missing",
+    "ERROR"
+  )))))
+}
+
+### Scrap functions
+# sum_rel <- function(occ, abb) {
+#   ifelse(occ == abb, 1, abb/occ)
+# }
+# convert_unit_names <- function(x) {
+#   switch(x,
+#     `1/L` = c("L", "cells/L"),
+#     stop("unit has not been implemented"))
+# }
---
+++ R/convert_abundance.R
@@ -0,0 +1,86 @@
+#' @title Calculate and convert between abundance values for ice core and planton net data
+#' @description Not ready conversion function
+#' @param data Data.frame containing the data to be converted
+#' @param ab_col Character vector specifying the columns containing abundance data
+#' @param ab_from The initial abundance unit. Options: "n" (number of individuals), "nsub" (number of individuals in a subsample), "1/L" (number of individuals per litre), "1/m3" (number of individuals per cubic metre), "1/m2" (number of individuals per square metre).
+#' @param ab_to The desired abundance unit. Options: "1/L" (number of individuals per litre), "1/m3" (number of individuals per cubic metre), "1/m2" (number of individuals per square metre).
+#' @param filtered Logical indicating whether the sample was filtered (\code{filtered = TRUE}) or subsampled (\code{filtered = FALSE}). See details.
+#' @param type Character argument specifying either the gear column (see \code{\link{export_metadata}}) or either of following: "icecore" or "net".
+#' @param cf Numeric giving the concentration factor connected to the volume difference when transforming water to ice (melted water volume/ice volume). Used only, if \code{type} indicates ice core. To remove the effect, set \code{cf = 1}.
+#' @param vol_col Character argument specifying the volume column to be used in calculations. Should be supplied in \strong{liters}.
+#' @param start_col Character argument specifying the name of the column in \code{data} for the section start for the ice core or plankton net in \strong{meters}.
+#' @param end_col Character argument specifying the name of the column in \code{data} for the section end for the ice core or plankton net in \strong{meters}.
+#' @details The function to calculate ind m^-2 values from ind is (ind*cf*1000*h)/V, where ind is number of individuals in a sample, cf the concentration factor, h height of ice-core section in m, and V melted volume in L.
+#' 
+#' The function is not ready and works only for "1/L" to "1/m2" conversions with very limited options. 
+#' 
+#' \itemize{
+#'   \item \strong{n} 
+#'   }
+#' 
+#' @author Mikko Vihtakari
+#' @export
+
+#data = k; ab_col <- c("abundance"); ab_from = "L"; ab_to = "m2"; filtered = TRUE; type = "icecore"; cf = 1; vol_col = NULL; start_col = "from"; end_col = "to"; unit_col = "unit"
+convert_abundance <- function(data, ab_col, ab_from = NULL, ab_to = NULL, filtered = NULL, type = "icecore", cf = 1, vol_col = NULL, start_col = "from", end_col = "to", unit_col = "unit") {
+
+  if(is.null(ab_from)) stop("Specify from argument. I.e the initial abundance unit.")    
+  if(is.null(ab_to)) stop("Specify to argument. I.e the desided abundance unit.")
+  if(is.null(filtered)) stop("Specify filtered argument. I.e whether samples were filtered or subsampled")
+  if(!is.null(vol_col)) if(type == "icecore" & any(data[vol_col] > 10)) warning("Volume needs to be supplied as liters. Very high volumes for an ice core. Are you sure?")
+  
+  ## Switches
+  
+  cm.switch <- NULL
+  
+  ### Conversions ####
+  
+  ### Filtered samples ####
+  if(filtered) {
+    
+    if(ab_from == "1/L" & ab_to == "1/m2") {
+      if(type == "icecore" & (any(data[[start_col]] > 10) | any(data[[end_col]] > 10))) {
+        cm.switch <- TRUE
+        message(paste0("start_col and end_col should be supplied as meters. Too high values (up to ", max(c(data[[start_col]], data[[end_col]])), "). Converted assuming that core distances are given as cm."))
+      data[[start_col]] <- data[[start_col]]/100
+      data[[end_col]] <- data[[end_col]]/100
+      }
+        
+      data[[ab_col]] <- indL2indm2(data = data, ab.cols = ab_col, sec.start = start_col, sec.end = end_col, cf. = cf)
+      data[[unit_col]] <- "1/m2"
+      
+      if(!is.null(cm.switch)) {
+      data[[start_col]] <- data[[start_col]]*100
+      data[[end_col]] <- data[[end_col]]*100
+      data
+      } else {
+      data  
+      }
+      
+    } else {
+      stop("Conversion from ", ab_from, " to ", ab_to, " has not been implemented yet")
+      }
+      
+  } else {
+  ### Nonfiltered samples ####  
+    stop("Nonfiltered samples have not been implemented yet")
+    
+    
+  }
+}
+
+## To calculate Ind/m2 values from Ind
+ind2indm2 <- function(data, ab.cols, V = "Volume", sec.start = "Sec.start", sec.end = "Sec.end", cf. = cf) { 
+  return(ind2indL(data, ab.cols, V, cf)*1000*((data[[sec.end]]-data[[sec.start]])/100))
+}
+
+
+## To calculate Ind/m2 values from Ind/L
+indL2indm2 <- function(data, ab.cols, sec.start, sec.end, cf.) { 
+  return((data[[c(ab.cols)]]*cf.*((data[[sec.end]]-data[[sec.start]]))))
+  }
+
+## To calcule Ind/L values from Ind
+ind2indL <- function(data, ab.cols = ab_col, V = vol_col, cf. = cf) {
+  return((cf.*data[ab.cols])/(data[[V]]))
+  }
