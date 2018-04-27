7759532afc75c0240f1285b560208fd5ec8b97e6
Mikko Vihtakari <mikko.vihtakari@gmail.com>
2018-04-27 16:45:21 +0200

    Updated stations list and added routine to fix names in datasets

---
+++ R/fix_station_names.R
@@ -0,0 +1,26 @@
+#' @title Match station names against standardized station names for Kongsfjorden and Ripfjorden
+#' @description Matches and fixes inconsistencies in station names for NPI's standard marine biological stations
+#' @param x A character vector or factor to be matched
+#' @return Returns a names character vector of corrected station names
+#' @author Mikko Vihtakari
+#' @export
+
+
+fix_station_names <- function(x) {
+
+ alternatives <- lapply(STATIONS$variations, function(l) {
+    trimws(unlist(strsplit(l, ";")))
+  })
+ 
+  #k <- tolower(x)[4]
+  new_names <- sapply(tolower(x), function(k) {
+    STATIONS[unlist(lapply(alternatives, function(l) k %in% l)), "station"]
+  })
+  
+  if(any(is.na(new_names))) {
+    warning("Could not find all station names.")
+  } else {
+    new_names
+  }
+  
+}
